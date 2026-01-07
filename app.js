53742d856fd0e92f2f19f87aca2912ff7151fd72
Hander Valverde <handervl@hotmail.com>
2026-01-07 16:35:33 -0500

    Deploy with html

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hola desde uso_jenkins (deploy en Docker)\n");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>CI / CD</h1>");
 });
 
 server.listen(PORT, () => {
