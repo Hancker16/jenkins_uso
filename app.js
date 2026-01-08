7cba97a5ededf718d4f1ac449e14416aa851435c
Hander Valverde <handervl@hotmail.com>
2026-01-08 10:28:14 -0500

    App.js without vulnerability

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const http = require("http");
 
 const PORT = process.env.PORT || 3000;
 
-const userInput = "console.log('hack')";
-eval(userInput);
-
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>Compilado</h1>");
