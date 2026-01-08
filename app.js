b3206ba30afb569db272c1e9eeda743d28ecf192
Hander Valverde <handervl@hotmail.com>
2026-01-08 09:08:29 -0500

    Test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>CI / CD</h1>");
+  res.end("<h1>Compilado</h1>");
 });
 
 server.listen(PORT, () => {
